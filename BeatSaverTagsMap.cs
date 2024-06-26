856c476ca60e1e89f220c683a81fc3376e5df693
Kinsi <kinsi55@users.noreply.github.com>
2024-06-26 18:41:12 +0200

    Create BeatSaverTagsMap.cs

---
+++ BeatSaverTagsMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SongDetailsCache {
+	public static class BeatSaverTagsMap {
+		public static readonly IReadOnlyDictionary<string, string> map = new Dictionary<string, string>() {
+			{ "tech", "Tech" },
+			{ "dance-style", "Dance" },
+			{ "speed", "Speed" },
+			{ "balanced", "Balanced" },
+			{ "challenge", "Challenge" },
+			{ "accuracy", "Accuracy" },
+			{ "fitness", "Fitness" },
+			{ "swing", "Swing" },
+			{ "nightcore", "Nightcore" },
+			{ "folk-acoustic", "Folk & Acoustic" },
+			{ "kids-family", "Kids & Family" },
+			{ "ambient", "Ambient" },
+			{ "funk-disco", "Funk & Disco" },
+			{ "jazz", "Jazz" },
+			{ "classical-orchestral", "Classical & Orchestral" },
+			{ "soul", "Soul" },
+			{ "speedcore", "Speedcore" },
+			{ "punk", "Punk" },
+			{ "rb", "R&B" },
+			{ "holiday", "Holiday" },
+			{ "vocaloid", "Vocaloid" },
+			{ "j-rock", "J-Rock" },
+			{ "trance", "Trance" },
+			{ "drum-and-bass", "Drum and Bass" },
+			{ "comedy-meme", "Comedy & Meme" },
+			{ "instrumental", "Instrumental" },
+			{ "hardcore", "Hardcore" },
+			{ "k-pop", "K-Pop" },
+			{ "indie", "Indie" },
+			{ "techno", "Techno" },
+			{ "house", "House" },
+			{ "video-game-soundtrack", "Video Game" },
+			{ "tv-movie-soundtrack", "TV & Film" },
+			{ "alternative", "Alternative" },
+			{ "dubstep", "Dubstep" },
+			{ "metal", "Metal" },
+			{ "anime", "Anime" },
+			{ "hip-hop-rap", "Hip Hop & Rap" },
+			{ "j-pop", "J-Pop" },
+			{ "dance", "Dance" },
+			{ "rock", "Rock" },
+			{ "pop", "Pop" },
+			{ "electronic", "Electronic" }
+		};
+	}
+}
